f9f47469b3e5f5680a3742710869137ee7887644
sanjaysatheesh <sanjaysatheesh417@gmail.com>
2025-07-29 13:49:48 -0700

    "added my first problem"

---
+++ problem1.py
@@ -0,0 +1,13 @@
+def comparetriplets(a, b):
+    # Write your code here
+    l=[]
+    for i in range(3):
+        l.append([a[i],b[i]])
+    r1=0
+    r2=0
+    for i in l:
+        if i[0]>i[1]:
+            r1+=1
+        elif i[1]>i[0]:
+            r2+=1
+    return r1,r2

dfe48ffdd081b4f852ef2721faacf0aefd31adbe
sanjaysatheesh <sanjaysatheesh417@gmail.com>
2025-07-29 22:29:22 -0700

    moved a file

--- algorithms/diagonal_difference.py
+++ algorithms/diagonal_difference.py
@@ -1,6 +1,7 @@
 #problem 2
 #https://www.hackerrank.com/challenges/diagonal-difference
-def diagonalDifference(arr):
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+def diagonaldifference(arr):
     # Write your code here
     d1=0
     d2=0
@@ -10,3 +11,5 @@ def diagonalDifference(arr):
         a-=1
         d2+=(arr[i][a])
     return abs(d1-d2)
+result = diagonaldifference(matrix)
+print("result:",result)

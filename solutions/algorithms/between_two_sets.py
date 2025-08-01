1afa81cc72b04fc39051457e0da93260c09e2115
sanjaysatheesh <sanjaysatheesh417@gmail.com>
2025-07-31 22:29:36 -0700

    new problem solution

--- solutions/algorithms/between_two_sets.py
+++ solutions/algorithms/between_two_sets.py
@@ -6,7 +6,18 @@ import sys
 
 def getTotalX(a, b):
     # Write your code here
-
+    res1=[]
+    res2=[]
+    for i in range(1,101):
+        if all(i%n==0 for n in a):
+            res1.append(i)
+        if all(l%i==0 for l in b):
+            res2.append(i)
+    res=0
+    for j in res1:
+        if j in res2:
+            res+=1
+    return res
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
--- testcases/test_algorithm/test_between_two_sets.py
+++ testcases/test_algorithm/test_between_two_sets.py
@@ -2,8 +2,8 @@ import pytest
 
 from solutions.algorithms.between_two_sets import getTotalX
 
-@pytest.mark.parametrize("a , b , expected", ([2,6] ,[24,36] , 2) , ([2,4] ,[16,32,96] , 3),
-                         ids=["TEST CASE 1", "TEST CASE 2"])
+@pytest.mark.parametrize("a,b,expected", [([2,6],[24,36],2) , ([2,4],[16,32,96],3)],
+ids=["TEST CASE 1", "TEST CASE 2"])
 
-def test_getTotalX(a, b, expected):
+def test_gettotalx(a, b, expected):
     assert getTotalX(a, b) == expected

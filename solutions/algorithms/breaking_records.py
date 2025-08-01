47b78895dc670f234ea2d2a1cf5d49713f535b14
sanjaysatheesh <sanjaysatheesh417@gmail.com>
2025-07-31 23:06:21 -0700

    new problem solution

---
+++ solutions/algorithms/breaking_records.py
@@ -0,0 +1,22 @@
+import math
+import os
+import random
+import re
+import sys
+
+def breakingRecords(scores):
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()

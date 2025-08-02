e5a428a77e35d16baf55c65edfa8b5ecc6393c4d
sanjaysatheesh <sanjaysatheesh417@gmail.com>
2025-08-01 19:47:00 -0700

    new problem solution

--- solutions/algorithms/subarray_division.py
+++ solutions/algorithms/subarray_division.py
@@ -6,7 +6,11 @@ import sys
 
 def birthday(s, d, m):
     # Write your code here
-
+    res=0
+    for i in range(len(s)):
+        if sum(s[i:i+m])==d:
+            res+=1
+    return res
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 

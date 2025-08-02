3fa57235892defc1635a07a82b3ed793c9fcad45
sanjaysatheesh <sanjaysatheesh417@gmail.com>
2025-08-01 18:17:36 -0700

    new problem solution

---
+++ solutions/algorithms/subarray_division.py
@@ -0,0 +1,27 @@
+import math
+import os
+import random
+import re
+import sys
+
+def birthday(s, d, m):
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = list(map(int, input().rstrip().split()))
+
+    first_multiple_input = input().rstrip().split()
+
+    d = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = birthday(s, d, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
